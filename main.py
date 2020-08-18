449c5ca41c42a4fbf908f76f1927bbfd6104cbdd
Matthew Weaver <31797463+Wibbo@users.noreply.github.com>
2020-08-18 16:53:15 +0100

    Added both lines to the same graph.

--- main.py
+++ main.py
@@ -3,48 +3,60 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-iterations = 10000
-results = np.zeros(iterations)
-change_mind = True
-prize = ''
+iterations = 1000
+change_results = np.zeros(iterations)
+stay_results = np.zeros(iterations)
+
+# change_mind = True
+change_prize = ''
+stay_prize = ''
 
 # Loop for the specified iterations...
 for i in range(iterations):
-    doors = np.array(['car', 'goat', 'goat'], dtype="U30")
-    np.random.shuffle(doors)
+    change_doors = np.array(['car', 'goat', 'goat'], dtype="U30")
+    stay_doors = np.array(['car', 'goat', 'goat'], dtype="U30")
+
+    np.random.shuffle(change_doors)
+    np.random.shuffle(stay_doors)   
     chosen_door = randint(0, 2)
 
-    if change_mind:
-        doors[chosen_door] = 'selected'
-        doors = np.sort(doors)
+    change_doors[chosen_door] = 'selected'
+    change_doors = np.sort(change_doors)
+    change_prize = change_doors[0]
 
-        # So now we have our chosen door. 
-        prize = doors[0]
-    else:
-        prize = doors[chosen_door]
+    stay_prize = stay_doors[chosen_door]
 
     # Have we guessed correctly? If so, update the results array.
-    if prize.startswith('car'):
-        results[i] = 1
+    if change_prize.startswith('car'):
+        change_results[i] = 1
+
+    if stay_prize.startswith('car'):
+        stay_results[i] = 1
 
 # Create a dataframe from the numpy array.
-ds = pd.DataFrame(results, columns=['Score'])
-ds['Mean'] = ds['Score'].expanding().mean()
+df_change = pd.DataFrame(change_results, columns=['Score'])
+df_change['Mean'] = df_change['Score'].expanding().mean()
 
-if change_mind:
-    title = 'Decide to change mind  ' + str(ds.iloc[iterations-1, 1])
-else:
-    title = 'Stick with 1st choice ' + str(ds.iloc[iterations-1, 1])   
+df_stay = pd.DataFrame(stay_results, columns=['Score'])
+df_stay['Mean'] = df_stay['Score'].expanding().mean()
+
+plt.title('The Monty Hall problem')
+plt.xlabel('Number of guesses')
+plt.ylabel('Average correct guesses')
+ax = df_change['Mean'].plot(label='Changed mind')
+df_stay['Mean'].plot(ax = ax, label='Did not change mind')
+plt.legend(loc='lower right')
+plt.show()
 
 
 
 # Plot the results.
-ds['Mean'].plot()
-plt.title(title)
-plt.xlabel('Number of guesses')
-plt.ylabel('Average correct guesses')
-plt.legend(loc='lower right')
-plt.show()
+#df_change['Mean'].plot()
+#plt.title('Hello')
+#plt.xlabel('Number of guesses')
+#plt.ylabel('Average correct guesses')
+
+#plt.show()
 
 
 
