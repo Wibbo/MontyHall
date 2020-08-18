bac604796dc2837181005d5595d895e9bb876dfa
Matthew Weaver <31797463+Wibbo@users.noreply.github.com>
2020-08-18 12:45:14 +0100

    Basic door selection for Monty Hall.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from random import randint
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-doors = np.zeros((1000,3), dtype=int)
+iterations = 1000
+results = np.zeros(iterations)
 
-for d in doors:
-    r = randint(0,2)
-    d[r] = 1
+for i in range(iterations):
+    doors = np.array(['car', 'goat', 'goat'])
+    np.random.shuffle(doors)
+    my_door_index = randint(0, 2)
+
+    if doors[my_door_index] == 'car':
+        results[i] = 1
+
+ds = pd.DataFrame(results, columns=['Score'])
+ds['Mean'] = ds['Score'].expanding().mean()
+print(ds)
+
+ds['Mean'].plot()
+plt.title('Single door, one choice')
+plt.xlabel('Number of guesses')
+plt.ylabel('Average correct guesses')
+plt.legend(loc='lower right')
+plt.show()
 
 
 
@@ -13,7 +31,16 @@ for d in doors:
 
 
 
-pass
+
+
+
+
+
+
+
+
+
+
 
 
 
